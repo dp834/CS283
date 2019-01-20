b103ef735b70c6c5fc0317882986fa4246133e70
dmp394 <dmp394@drexel.edu>
2019-01-19 23:04:04 -0500

    Tee hurts my files

---
+++ Homework/2/tee.c
@@ -0,0 +1,12 @@
+#include "csapp.h"
+
+int main(int argc, char *argv[]){
+	// -a option appends, no option overwrites
+	int aFlag = 0;
+	char c;
+
+	while ((c = getopt(argc, argv, ":a"))!= -1){
+		
+	}
+	return 0;
+}
