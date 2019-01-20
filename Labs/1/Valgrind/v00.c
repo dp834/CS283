538145ee289af84fd1d1a1b9705d679d1ae79f5a
dmp394 <dmp394@drexel.edu>
2019-01-19 20:28:15 -0500

    completed lab1

--- Labs/1/Valgrind/v00.c
+++ Labs/1/Valgrind/v00.c
@@ -1,14 +1,14 @@
 #include <stdlib.h>
 
-void f ()
- {
-  int *x;
-  x = malloc(10 * sizeof(int));
-  x[10] = 0;
- }
+void f (){
+	int *x;
+	x = malloc(10 * sizeof(int));
+	x[9] = 0;//x[10] is out of bounds since we zero index
+	//x isn't returned or freed so its a memory leak
+	free(x);
+}
 
-int main ()
- {
-  f();
-  return 0;
- }
+int main (){
+	f();
+	return 0;
+}
--- Labs/1/Valgrind/v06.c
+++ Labs/1/Valgrind/v06.c
@@ -1,16 +1,12 @@
 #include <stdio.h>
 
-int foo (x)
-int x;
- {
-  if(x < 10)
-   {
-    printf("x is less than 10\n");
-   }
- }
+int foo (int x){
+	if(x < 10){
+		printf("x is less than 10\n");
+	}
+}
 
-int main()
- {
-  int y;
-  foo(y);
- }
+int main(){
+	int y = 4;
+	foo(y);//originally passed uninitialized
+}
