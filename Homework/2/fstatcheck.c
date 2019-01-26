a0585add887e489d13f188457dd2cfac957730c4
dmp394 <dmp394@drexel.edu>
2019-01-26 11:48:47 -0500

    A bunch of files got deleted

---
+++ Homework/2/fstatcheck.c
@@ -0,0 +1,24 @@
+#include "csapp.h"
+
+int main(int argc, char**argv) {
+	struct stat stat;
+	char *type, *readok;
+
+	Fstat((int)((*argv[1])-'0'), &stat);
+	if (S_ISREG(stat.st_mode)) {
+		type = "regular";
+	} else if (S_ISDIR(stat.st_mode)) {
+		type = "directory";
+	} else {
+		type = "other";
+	}
+
+	if ((stat.st_mode & S_IRUSR)) {
+		readok = "yes";
+	} else {
+		readok = "no";
+	}
+
+	printf("type: %s, read: %s\n", type, readok);
+	exit(0);
+}
